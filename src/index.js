07c08139119aac2936124df4be4ae4881e09ee3a
Rakka Alhazimi <rakkakeren@gmail.com>
2024-06-27 15:36:43 +0700

    added: google drive scope

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?';
-const scope = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email';
+const scopes = [
+  'https://www.googleapis.com/auth/calendar',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/drive.file'
+]
+const scope = scopes.join(' ');
 
+  
+  
 async function googleAuth() {
   let url = GOOGLE_AUTH_URL + new URLSearchParams({
     redirect_uri: 'http://localhost:3000/api/auth/callback/google',
