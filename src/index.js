7428f955391e0b9941e18d97875e5cc1b8a95438
Rakka Alhazimi <rakkakeren@gmail.com>
2024-11-20 14:30:50 +0700

    added: get google_client_id from json file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import * as fs from 'fs'
+
+
+fs.readFileSync('auth.json');
+let buffer = fs.readFileSync('auth.json');
+let auth = JSON.parse(buffer.toString());
+
 const GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?';
 const scopes = [
   'https://www.googleapis.com/auth/calendar',
@@ -12,7 +19,7 @@ const scope = scopes.join(' ');
 async function googleAuth() {
   let url = GOOGLE_AUTH_URL + new URLSearchParams({
     redirect_uri: 'http://localhost:3000/api/auth/callback/google',
-    client_id: '<Your google client ID>',
+    client_id: auth.GOOGLE_CLIENT_ID,
     access_type: 'offline',
     response_type: 'code',
     scope: scope,
